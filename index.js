a45b6fa50022450133ba3424466e642bfd53af46
guygubaby <guygubaby@gmail.com>
2023-09-03 10:48:09 +0800

    feat: update deps

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ module.exports = {
   extends: [
     '@antfu',
   ],
+  rules: {
+    "vue/comma-dangle": "off",
+    "vue/space-unary-ops": "off",
+    "vue/dot-location": "off",
+    "vue/space-infix-ops": "off",
+    "vue/object-curly-newline": "off",
+    "antfu/no-ts-export-equal": "off",
+    "antfu/top-level-function": "off",
+    "antfu/no-cjs-exports": "off"
+  }
 }
